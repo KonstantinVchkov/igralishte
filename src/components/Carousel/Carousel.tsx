4773b79d7290b19e2dcf0539be53d262dcc344ac
Konstantin Vckov <kvckov@yahoo.com>
2023-11-12 09:43:58 +0100

    mading the carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -3,43 +3,28 @@ import style from "./style.module.css";
 import Carousel from "react-bootstrap/Carousel";
 // import ExampleCarouselImage from '';
 interface IHomeCarousel {
-  img:string;
-  title:string;
-  price:string;
+  img: string;
+  title: string;
+  price: string;
 }
-const HomeCarousel = () => {
+const HomeCarousel = ({ img, title, price }: IHomeCarousel) => {
   return (
-    // <div className={style.HomeCarousel}>
     <Carousel fade>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <img src="/images/banner-images/girl-top-banner.jpg" alt="" />
-        <Carousel.Caption>
-
-        </Carousel.Caption>
+        <img src={img} alt="" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
-        <img src="/images/banner-images/girl-top-banner.jpg" alt="" />
+        <img src={img} alt="" />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <img src="/images/banner-images/girl-top-banner.jpg" alt="" />
+        <img src={img} alt="" />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    // </div>
   );
 };
 
