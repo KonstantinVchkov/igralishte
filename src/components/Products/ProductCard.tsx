db495fbf2f4283e4f029ab55e6838a220d495804
Konstantin Vckov <kvckov@yahoo.com>
2023-11-21 09:29:42 +0100

    making of product card

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import style from "./style.module.css"
-
-const ProductCard = () => {
+import React from "react";
+import style from "./style.module.css";
+export interface IProductCardProps {
+  brand: string;
+  name: string;
+  category: string;
+  color: string;
+  size: string;
+  sizeDescription: string;
+  price: string;
+  discount: string;
+  material: string[];
+  condition: string;
+  odrzuvanje: string[];
+  tags: string[];
+  images: string[];
+}
+export type ProductProps = {
+  props: IProductCardProps;
+};
+const ProductCard = ({ props }: ProductProps) => {
   return (
     <div className={style.ProductCard}>
-
+      <div className={style.firstSection}>
+        <h1>{props.name}</h1>
+        <div className={style.ImagesSection}>
+          {props.images.map((img, index) => (
+            <img key={index} src={img} alt="" />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
