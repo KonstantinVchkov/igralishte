b5a7d55219ea7f30089e18035f6c59bff420415a
Konstantin <kvckov@yahoo.com>
2023-12-14 23:44:20 +0100

    search filter changed

--- src/components/Header/SearchFilter.tsx
+++ src/components/Header/SearchFilter.tsx
@@ -5,29 +5,51 @@ import style from "./style.module.css";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
 import { IProductProps, ISearchMenu } from "@/types/ProjectTypes";
-import router from "next/router";
 import axios from "axios";
-import { GetServerSideProps } from "next";
-import Product from "../Products/Product";
-const SearchFilter = ({
-  show,
-  handleClose,
-  products,
-  handleFilter,
-}: ISearchMenu) => {
-  console.log("Products in SearchFilter:", products);
-  const [searchedProduct, setSearchProducts] = useState<string>("");
-  const searchProducts = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
-    setSearchProducts(searchTerm);
-    // handleFilter?.(searchTerm);
-  };
 
+import Product from "../Products/Product";
+import Pagination from "../Pagination/Pagination";
+import { getPaginatedProducts } from "@/utils/paginationFunction";
+import Link from "next/link";
+const SearchFilter = ({ show, handleClose }: ISearchMenu) => {
+  const [searchedProduct, setSearchProducts] = useState<string>("");
+  const [filteredProducts, setFilteredProducts] = useState<IProductProps[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const paginatedProducts = getPaginatedProducts(
+    filteredProducts,
+    currentPage,
+    itemsPerPage
+  );
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const searchProducts = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let searchTerm = e.target.value;
+    if (searchTerm.length > 0) {
+      searchTerm = searchTerm.charAt(0).toLowerCase() + searchTerm.slice(1);
+    }
+    setSearchProducts(searchTerm);
+  };
   const submitSearchproducts = (e: any) => {
     e.preventDefault();
-    router.push(
-      `http://localhost:3000/searchProducts?category=${searchedProduct}`
-    );
+    axios
+      .get(`http://localhost:3001/products`)
+      .then((res) => {
+        const filtered = res.data.filter(
+          (product: any) =>
+            product.category.charAt(0).toLowerCase() +
+              product.category.slice(1) ===
+            searchedProduct
+        );
+        setFilteredProducts(filtered);
+      })
+      .catch((error) =>
+        console.error("Error fetching filtered products:", error)
+      );
+  };
+  const handleProductClick = () => {
+    handleClose();
+    setSearchProducts("");
+    setFilteredProducts([]);
   };
   return (
     <>
@@ -37,7 +59,7 @@ const SearchFilter = ({
         placement="end"
         className={style.fullWidthOffcanvas}
       >
-        <Offcanvas.Body className="color-black">
+        <Offcanvas.Body className="color-black mx-auto d-flex flex-wrap">
           <Form onSubmit={submitSearchproducts}>
             <Form.Group className="d-flex align-items-center justify-content-center">
               <FontAwesomeIcon
@@ -55,8 +77,27 @@ const SearchFilter = ({
               />
             </Form.Group>
           </Form>
-          {products &&
-            products.map((item) => <Product key={item.id} {...item} />)}
+          {paginatedProducts.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className={style.filteredProduct}
+                onClick={handleProductClick}
+              >
+                <Link href={`http://localhost:3000/products/${item.id}`}>
+                  <Product {...item} />
+                </Link>
+              </div>
+            );
+          })}
+          {filteredProducts.length > 0 && (
+            <Pagination
+              itemsPerPage={itemsPerPage}
+              totalItems={filteredProducts.length}
+              paginate={paginate}
+              currentPage={currentPage}
+            />
+          )}
         </Offcanvas.Body>
       </Offcanvas>
     </>
